ee933922ab2ec2d054834717ad0f6135113c48d3
SvetaKozyreva <svtkozyreva@gmail.com>
2024-09-12 21:02:07 +0300

    Add project files.

---
+++ Lab2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random o = new Random();
+            List<int> chisla = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                chisla.Add(o.Next(1, 20));
+                Console.Write("{0} ", chisla[i]);
+            }
+        }
+    }
+}
